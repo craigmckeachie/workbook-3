4c98d772c252af77b7240f12d69d295862f04002
Craig McKeachie <craigdmckeachie@gmail.com>
2025-04-22 14:36:34 -0400

    file input stream example

---
+++ GagaApp/src/main/java/App.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("just-dance.txt");
+            Scanner scanner = new Scanner(fis);
+
+            String line;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+//            throw new RuntimeException(e);
+        }
+
+
+    }
+}
